fc24556f6ef88ee699a361b9a4008baff05f1eb9
G Shiva Ganesh <74356635+G-ShivaGanesh@users.noreply.github.com>
2021-10-09 11:23:09 +0530

    Day 3 assignment submitted

---
+++ Day3Assignment.py
@@ -0,0 +1,42 @@
+
+"""Day3Assignment.ipynb
+
+Original file is located at
+    https://colab.research.google.com/drive/1o-r7P1Gfzt4eQ0BI1xWrRHrGiRoF0dFE
+"""
+
+# First input
+# ------------ 
+
+i=1000
+
+if i>0 and i<=1000:
+  print("Safe to land")
+elif i>1000 and i<=5000:
+  print("Bring down to 1000")
+else:
+  print("Turn Around")
+
+#Second input
+# ------------
+
+i=4500
+
+if i>0 and i<=1000:
+  print("Safe to land")
+elif i>1000 and i<=5000:
+  print("Bring down to 1000")
+else:
+  print("Turn Around")
+
+#Third input
+# ------------
+
+i=6500
+
+if i>0 and i<=1000:
+  print("Safe to land")
+elif i>1000 and i<=5000:
+  print("Bring down to 1000")
+else:
+  print("Turn Around")
